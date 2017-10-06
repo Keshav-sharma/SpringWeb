ef2a2515b7177ce0a357affc74bb4b7f5d39e4ef
Keshav <sharmakeshav1032@gmail.com>
2017-10-07 00:13:56 +0530

    pilot push

---
+++ src/main/java/com/example/test/SpringHello/App.java
@@ -0,0 +1,13 @@
+package com.example.test.SpringHello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
